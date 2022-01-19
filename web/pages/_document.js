9f346de7c34342780173230f34e683e30e53816f
Will Kelly <william.kelly20@btinternet.com>
2022-01-19 12:18:58 +0000

    Converting to monorepo

---
+++ web/pages/_document.js
@@ -0,0 +1,44 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <title>IKONO</title>
+          <meta
+            name="description"
+            content="A large collection of high quality SVG icons with an MIT license"
+          />
+          <meta name="theme-color" content="#2563EB" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
