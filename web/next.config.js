573e45bf1c81a964e772f9fa0c428fb4598a6455
Will <wdk1g19@soton.ac.uk>
2023-10-09 21:16:59 +0100

    Set appropriate security headers

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,31 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextSafe = require('next-safe');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development'
 });
 
 module.exports = withPWA({
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: nextSafe({
+          isDev,
+          contentSecurityPolicy: {
+            'font-src': "'self' fonts.gstatic.com",
+            'style-src': "'self' fonts.googleapis.com",
+            'script-src': ["'self'", 'va.vercel-scripts.com'],
+            'upgrade-insecure-requests': [],
+            'prefetch-src': false
+          }
+        })
+      }
+    ];
+  }
 });
