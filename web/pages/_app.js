ca65e3e8ec23aa6fab05f2bf5ad8341cdc48cb0a
wkelly1 <26680240+wkelly1@users.noreply.github.com>
2023-10-26 22:08:45 +0100

    Add vercel analytics

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,8 +1,14 @@
 import { useEffect } from "react";
 import "tailwindcss/tailwind.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
