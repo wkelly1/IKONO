95e13a3bc3c7c90b51036a2948095a43d35ca087
wkelly1 <26680240+wkelly1@users.noreply.github.com>
2023-10-26 22:08:44 +0100

    Add email client UI

--- web/pages/ui/index.js
+++ web/pages/ui/index.js
@@ -12,6 +12,7 @@ import ui4 from "../../public/images/ui/ui4.png";
 import ui5 from "../../public/images/ui/ui5.png";
 import ui6 from "../../public/images/ui/ui6.png";
 import ui7 from "../../public/images/ui/ui7.png";
+import ui8 from "../../public/images/ui/ui8.png";
 
 function UIImage({ src, date, title }) {
   const [open, setOpen] = useState(false);
@@ -156,6 +157,7 @@ export default function Home() {
             <UIImage src={ui5} title="Signup Page" date="Jan 24, 2023" />
             <UIImage src={ui6} title="Dashboard" date="Jan 24, 2023" />
             <UIImage src={ui7} title="Web Dashboard" date="Jan 24, 2023" />
+            <UIImage src={ui8} title="Email Client" date="Jan 25, 2023" />
           </div>
         </main>
       </div>
