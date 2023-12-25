41030bc90d8d0f5bc2b0e1c4c96175984b9b92db
wkelly1 <26680240+wkelly1@users.noreply.github.com>
2023-12-25 20:31:57 +0000

    Move head to _app

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -9,16 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="IKONO icons are a large collection of high quality, hand-crafted SVG icons with an MIT license. They are accompanied by their own Figma plugin and React library."
-          />
-          <meta name="theme-color" content="#2563EB" />
-        </Head>
+        <Head></Head>
         <body
           style={{ width: '100vw', overflowX: 'hidden', margin: 0, padding: 0 }}
         >
