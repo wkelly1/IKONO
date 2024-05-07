d58856d30d01d5640cedcbaa66593034248cac40
wkelly1 <26680240+wkelly1@users.noreply.github.com>
2024-05-07 22:51:36 +0100

    Ensure web version.json gets bumped along with react

--- scripts/bump_all.ts
+++ scripts/bump_all.ts
@@ -1,15 +1,28 @@
+// This script will update the version number of all sub-packages
 import mainPackageJson from '../package.json';
 import * as fs from 'fs';
 import * as path from 'path';
 
-console.log(mainPackageJson);
-
-const location = path.resolve('react', 'package.json');
+// Update version for react library
+const reactLocation = path.resolve('react', 'package.json');
 fs.writeFileSync(
-  location,
+  reactLocation,
   JSON.stringify(
     {
-      ...JSON.parse(fs.readFileSync(location).toString()),
+      ...JSON.parse(fs.readFileSync(reactLocation).toString()),
+      version: mainPackageJson.version
+    },
+    null,
+    3
+  )
+);
+
+// Update version.json in web package
+const webLocation = path.resolve('web', 'version.json');
+fs.writeFileSync(
+  webLocation,
+  JSON.stringify(
+    {
       version: mainPackageJson.version
     },
     null,
