3547e385bd957b922b33e1d7921f1c8c08b6f54d
wkelly1 <26680240+wkelly1@users.noreply.github.com>
2024-08-23 12:13:52 +0100

    Update default dark/light mode toggle to show correct icon

--- web/components/Navbar/DarkModeToggle.tsx
+++ web/components/Navbar/DarkModeToggle.tsx
@@ -38,7 +38,7 @@ export default function ThemeButton({ className }: ThemeButtonProps) {
         }}
         className={twMerge(className, 'rounded-full')}
       >
-        {currentTheme === 'dark' ? (
+        {currentTheme === 'light' ? (
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
             <g fill="none">
               <circle
