2e96959101107ac34a3b5643afcd7084c2b2e1af
wkelly1 <26680240+wkelly1@users.noreply.github.com>
2025-09-14 22:05:12 +0100

    Update icon types to be optional

--- react/src/types.ts
+++ react/src/types.ts
@@ -2,7 +2,7 @@ export interface IconProps extends React.ComponentPropsWithRef<'svg'> {
   /** A color formatted string used to change the colour of the icon.
    * If set to `currentColor` then it will inherit the colour from
    * the text colour. */
-  color: string;
+  color?: string;
   /** The size of the icon. */
-  size: 'sm' | 'base';
+  size?: 'sm' | 'base';
 }
